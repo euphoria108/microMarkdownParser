6eaa2c857ea657005be829aef1b453c50c3fa810
euphoria <sunraiziarnusg@gmail.com>
2018-08-20 18:15:12 +0900

    added implementation of parse function

--- parser.py
+++ parser.py
@@ -1,60 +1,64 @@
 import re
 import warnings
+from collections import OrderedDict
 
 #正規表現定義エリア
-rules = {}
-rules['table block'] = re.compile(r'''
+block_rules = OrderedDict()
+block_rules[table] = re.compile(r'''
     ^(.*)           
     \|               #symbol that distinguishes this block is a table           
     (.*)
 ''', re.VERBOSE)
-rules['blockquote'] = re. compile(r"""
+block_rules[blockQuote] = re. compile(r"""
     (&gt;|\>)        #symbol that distinguishes this line is a start of blockquote
     (.*)
 """, re.VERBOSE)
-rules['linebreak'] = re.compile(r'\s{2,}\n')
-rules['headers'] = re.compile(r"""
-    (\#+)            #header symbol
-    (.*)
-""", re.VERBOSE)
-rules['tag_block'] = re.compile(r"""
+block_rules[taggedBlock] = re.compile(r"""
     ^(\<p\>)         #start tag
     (.*)
     (\<\/p\>)$       #end tag
 """, re.VERBOSE)
-rules['links'] = re.compile(r"""
+block_rules[normalBlock] = re.compile(r"""
+    (.*?)\n\s*\n
+""", flags=(re.VERBOSE|re.MULTILINE|re.ADDDOT))
+rules[lineBreak] = re.compile(r'\s{2,}\n')
+rules[headers] = re.compile(r"""
+    (\#+)            #header symbol
+    (.*)
+""", re.VERBOSE)
+rules[links] = re.compile(r"""
     \[([^\[]+)\]\(([^\)]+)\)
 """, re.VERBOSE)
-rules['bold'] = re.compile(r"""
+rules[boldFont] = re.compile(r"""
     (\*\*|__)(.*?)\1
 """, re.VERBOSE)
-rules['emphasis'] = re.compile(r"""
+rules[emphasizedFont] = re.compile(r"""
     (\*|_)(.*?)\1
 """, re.VERBOSE)
-rules['delete'] = re.compile(r"""
+rules[deletedFont] = re.compile(r"""
     \~\~(.*?)\~\~
 """, re.VERBOSE)
-rules['quote'] = re.compile(r"""
+rules[inlineQuote] = re.compile(r"""
     \:\"(.*?)\"\:
 """, re.VERBOSE)
-rules['inline code'] = re.compile(r"""
+rules[inlineCode] = re.compile(r"""
     `(.*?)`
 """, re.VERBOSE)
-rules['ul lists'] = re.compile(r"""
+rules[ulLists] = re.compile(r"""
     \s*\*\s+(.*)
 """, re.VERBOSE)
-rules['ol lists'] = re.compile(r"""
+rules[olLists] = re.compile(r"""
     \s*[0-9]+\.\s+(.*)
 """, re.VERBOSE)
-rules['horizontal rule'] = re.compile(r"""
+rules[horizontalRule] = re.compile(r"""
     ^((\-\s?){3,}|
     (\*\s?){3,}|
     (\_\s?){3,})$
 """, re.VERBOSE)
-rules['code block'] = re.compile(r"""
+rules[codeBlock] = re.compile(r"""
     \s{4,}\w*
 """, re.VERBOSE)
-rules['definition area'] = re.compile(r"""
+rules[definitionBlock] = re.compile(r"""
     \[
     [^\[]*
     \]
@@ -65,6 +69,7 @@ rules['definition area'] = re.compile(r"""
 
 class MarkdownParser:
     def __init__(self):
+        pass
 
     def title_handler(self):
         pass
@@ -80,8 +85,66 @@ class baseObject:
         self.parsed_data = []
         parse()
 
+    def reset(self):
+        self.start_pos = 0
+
     def parse(self, parse=True):
         if not parse:
             return
         
+        i = self.start_pos
+        rawdata = self.rawdata
+        while i < len(rawdata):
+            for rule in rules:
+                if rule.match(rawdata,i):
+                    start_matched = rule.match(rawdata,i).start()
+                    end_matched = rule.match(rawdata,i).end()
+                    if start_matched > i:
+                        self.parsed_data.append(rawdata[i:start_matched])
+                    self.parsed_data.append(rule[0](rawdata[start_matched:end_matched]))
+                    i = end_matched
+
+    def expandToHTML(self):
+        #expand parced objects to HTML recursively        
+        pass
+
+class root(baseObject):
+        
+class table(baseObject):
+
+class blockQuote(baseObject):
+
+class lineBreak(baseObject):
+
+class headers(baseObject):
+
+class links(baseObject):
+
+class taggedBlock(baseObject):
+    def parse(self):
+        #We don't parse any words in tagged block.
+        return
+
+class normalBlock(baseObject):
+
+class boldFont(baseObject):
+
+class emphasizedFont(baseObject):
+
+class deletedFont(baseObject):
+
+class inlineQuote(baseObject):
+
+class inlineCode(baseObject):
+
+class ulLists(baseObject):
+
+class olLists(baseObject):
+
+class horizontalRule(baseObject):
+    
+class codeBlock(baseObject):
+
+class definitionBlock:
+
 
